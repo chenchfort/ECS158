9dade9d509317f3e1105a9d13dbad76cfd6ae6a6
Charles Chen <cyuchen@ucdavis.edu>
2016-04-14 23:00:58 -0700

    Update Problem1.R

---
+++ HW2/Problem1.R
@@ -0,0 +1,9 @@
+parquad <- function(u, a, val)
+{
+ require(parallel)    
+ idx <- splitIndices(nrow(u), myinfo$nwrkrs)[[myinfo$id]]
+ rdsmlock("vallock")
+ val[1,1] <- val[1,1] + t(u[,]) %​*% a[,idx] %*​% u[idx,]
+ rdsmunlock("vallock")
+ 0
+}
