4e95958b132e940462b17bbd828f6b1f8dbfe4e4
Eric Tan <ejtan@pc35.cs.ucdavis.edu>
2016-04-14 21:08:41 -0700

    Problem1.R

---
+++ HW2/Problem1.R
@@ -0,0 +1,20 @@
+# ECS 158, Problem1.R
+# Computes the quadratic for u'Au
+# Uses Rdsm
+
+# parquad(u, a val)
+# Input : u - vectors
+#         a - Matrix
+#         val - return variable
+parquad <- function(u, a, val)
+{
+  require(parallel)
+
+  u_t <- t(u) # transpose u
+
+  index <- splitIndices(nrow(u), myinfo$nwrkrs)[[myinfo$id]]
+  val[index,] <- u_t[index,] %*% A[,]
+
+  val[index,] <- A[,] %*% u[index,]
+  return(0)
+}
