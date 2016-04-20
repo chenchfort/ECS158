e33a5d1865ce3845a1af73a49725c47b04a98a36
ejtan <ejtan@ucdavis.edu>
2016-04-19 22:07:10 -0700

    Added files via upload
    Got it to go as fast as I could. Changed the code so that goto wasn't used, but used a function call, so it can still be faster.

--- HW2/Problem2.cpp
+++ HW2/Problem2.cpp
@@ -1,29 +1,24 @@
-/* ECS 158
- * Problem 2
- */
-
 #include <iostream>
 #include <random>
+#include <chrono>
 #include <omp.h>
 
-#define N 10
-#define K 2
+#define N 10000
+#define K 1
 
 using namespace std;
 
 /* Seed RNG with constant seed */
+unsigned seed = chrono::system_clock::now().time_since_epoch().count();
 mt19937 engine;
 uniform_real_distribution<float> distribution(0.0, 1.0);
 
-bool is_bright(float *pix, int r, int c, int n, int k, float thresh)
-{
-  for (int i = r; i < (r + k); i++)
-    if (pix[i * n + c] < thresh)
-      return false;
 
-  for (int i = r; i < (r + k); i++)
-    for (int j = c; j < (c + k); j++)
-      if (pix[i * n + j] < thresh)
+bool is_brights(float *pix, int i, int j,int n, int k, float thresh)
+{
+  for (int p = i; p < i + k; p++)
+    for (int q = j; q < j + k; q++)
+      if (pix[p * n + q] < thresh)
         return false;
 
   return true;
@@ -33,46 +28,45 @@ int brights(float *pix, int n, int k, float thresh)
 {
   int count = 0;
 
-  // Parallelize nested for loop
-  // Works 95% of the time
-  //#pragma omp parallel for collapse(2)
-  for (int i = 0; i < (n - k); i++) // rows
-    for (int j = 0; j < (n - k); j++) // col
-      if (pix[i * n + j] >= thresh)
-        if (is_bright(pix, i, j, n, k, thresh))
-          count++;
+  #pragma omp parallel for reduction(+:count) collapse(2) schedule(guided)
+  for (int i = 0; i < n - K + 1; i++)
+    for (int j = 0; j < n - k + 1; j++)
+      if (pix[i * n + j] > thresh) // Check if pixle is already above thresh
+        if (is_brights(pix, i, j, n, k, thresh)) // May be rewritten so no funnction is called
+          count +=1;
 
   return count;
 }
+
 
 int main(void)
 {
   float *pix = new float[N * N];
   float thresh = 0.5;
-  int count;
+  int bright_ctr, pix_ctr = 0;
+  chrono::time_point<chrono::system_clock> start, end;
+  chrono::duration<double> time_elapsed;
 
-  // Randomly set test matrix
-  // Matrix always same due to seed
   for (int i = 0; i < (N * N); i++)
+  {
     pix[i] = distribution(engine);
 
-  // Print matrix for verifacation
-  for (int i = 0; i < N; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
-      if (pix[i * N + j] >= thresh)
-        cout << "* ";
-      else
-        cout << "- ";
-    }
-    cout << endl;
+    if (pix[i] > thresh)
+      pix_ctr++;
   }
+
+  start = chrono::system_clock::now();
+
+  bright_ctr = brights(pix, N, K, thresh);
+
+  end = chrono::system_clock::now();
+
+  time_elapsed = end - start;
+
+  cout << "Bright pixles  : " << pix_ctr << endl;
+  cout << "From brights() : " << bright_ctr << endl;
+  cout << "Time taken     : " << time_elapsed.count() << "s" << endl;
   cout << endl;
-
-  count = brights(pix, N, K, thresh);
-
-  cout << "Number of Bright Spots : " << count << endl;
 
   delete [] pix;
 
