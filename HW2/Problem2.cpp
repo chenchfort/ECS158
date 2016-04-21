9154ce5bd25bd7025352b396504384bd188723f4
Charles Chen <cyuchen@ucdavis.edu>
2016-04-20 18:09:44 -0700

    Test both

--- HW2/Problem2.cpp
+++ HW2/Problem2.cpp
@@ -24,7 +24,7 @@ bool is_brights(float *pix, int i, int j,int n, int k, float thresh)
   return true;
 }
 
-int brights(float *pix, int n, int k, float thresh)
+int brights2(float *pix, int n, int k, float thresh)
 {
   int count = 0;
 
@@ -38,6 +38,22 @@ int brights(float *pix, int n, int k, float thresh)
   return count;
 }
 
+int brights(float *pix, int n, int k, float thresh)
+{
+  int count = 0;
+  #pragma omp parallel for reduction(+:count) collapse(1) schedule(guided)
+  for (int i = 0; i < n - k+1; i++)
+    for (int j = 0; j < n - k+1; j++) {
+      for (int p = i; p < i+k; p++)
+        for (int q = j; q < j+k; q++)
+          if (pix[p*n + q] < thresh)
+            goto fall;
+          count+=1;
+        fall: ;
+    }   
+
+  return count;
+}
 
 int main(void)
 {
