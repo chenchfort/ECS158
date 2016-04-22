96672ceb90f7544dc0cbfa267d9fc5ebd6dee5ed
willxcience <wilwu@ucdavis.edu>
2016-04-21 22:13:40 -0700

    Update Problem2.cpp
    PLEASE CHECK CAREFULLY BEFORE U SUBMIT

--- HW2/Problem2.cpp
+++ HW2/Problem2.cpp
@@ -2,26 +2,26 @@
 
 using namespace std;
 
-bool is_brights(float *pix, int i, int j,int n, int k, float thresh)
+inline bool is_brights(const float *pix, const int i, const int j, const int n,const int k, const float thresh) 
 {
-  for (int p = i; p < i + k; p++)
-    for (int q = j; q < j + k; q++)
-      if (pix[p * n + q] < thresh)
-        return false;
-
-  return true;
+	for (int p = i; p < i + k; p++)
+		for (int q = j; q < j + k; q++)
+			if (pix[p * n + q] < thresh)
+				return false;
+	return true;
 }
 
-int brights(float *pix, int n, int k, float thresh)
+
+int brights(float *pix, int n, int k, float thresh) 
 {
   int count = 0;
-
+  const int s = n - k + 1;
   #pragma omp parallel for reduction(+:count) collapse(2) schedule(guided)
-  for (int i = 0; i < n - K + 1; i++)
-    for (int j = 0; j < n - k + 1; j++)
-      if (pix[i * n + j] > thresh) // Check if pixle is already above thresh
+  for (int i = 0; i < s; i++)
+    for (int j = 0; j < s; j++)
+      if (pix[i * n + j] >= thresh) // Check if pixle is already above thresh
         if (is_brights(pix, i, j, n, k, thresh))
-          count++;
+          count+=1;
 
   return count;
 }
