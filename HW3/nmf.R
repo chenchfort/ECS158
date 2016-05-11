d46433c07fc496b1e23ab43c536a0f9dfaa8e9fa
Willoop <asdf751953@gmail.com>
2016-05-10 22:38:08 -0700

    Update nmf.R

--- HW3/nmf.R
+++ HW3/nmf.R
@@ -1,5 +1,6 @@
 nmfgpu <- function(a,k)
 {
+  #following code depends on the machine
   system("/usr/local/cuda/bin/nvcc -g -Xcompiler -fPIC -c nmfgpur.cu -arch=sm_13 -I/usr/include/R")
   system("R CMD SHLIB nmfgpur.o -lcudart -L/usr/local/cuda/lib64")
   dyn.load("nmfgpur.so")
