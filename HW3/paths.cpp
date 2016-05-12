67dccb2339a07a6fdd5faf0781e9f1a252f4f3fb
Eric Tan <ejtan@pc43.cs.ucdavis.edu>
2016-05-11 18:07:51 -0700

    Parallelized for loop, made a few changes

--- HW3/paths.cpp
+++ HW3/paths.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <omp.h>
 
-
 using namespace std;
+
+int block;
 
 void check_path(int *adjm, int n, int k, int *paths, int *numpaths, int row, int &depth, int *visited)
 {
 	// Base case
-	if (depth == k)
-	{
+	if (depth == k) {
 		#pragma omp critical
 		{
 			for (int *ptr = paths + (*numpaths) * (k + 1), i = 0; i <= depth; i++, ptr++)
-			{
-				
 				*ptr = visited[i];
-			}
+
 			(*numpaths)++;
 		}
 		return;
@@ -23,16 +21,12 @@ void check_path(int *adjm, int n, int k, int *paths, int *numpaths, int row, int
 
 	for (int col = 0; col < n; col++)
 	{
-		if (adjm[row * n + col] != 1 && col == (n - 1))
-			return;
-
-		if (adjm[row * n + col] == 1)
-		{
+		if (adjm[row * n + col] == 1) {
 			depth++;
 			visited[depth] = row;
 			check_path(adjm, n, k, paths, numpaths, col, depth, visited);
 			depth--;
-			// Avoid duplicates
+      // avoid double count
 			if (depth == k - 1)
 				break;
 		}
@@ -42,17 +36,19 @@ void check_path(int *adjm, int n, int k, int *paths, int *numpaths, int row, int
 void findpaths(int *adjm, int n, int k, int *paths, int *numpaths)
 {
 	*numpaths = 0;
-	// Iterate to check through all the rows and col
-
-#pragma omp parallel shared(paths, numpaths)
+  #pragma omp parallel shared(paths, numpaths)
 	{
 		int me = omp_get_thread_num();
 		int nth = omp_get_num_threads();
-		int block = n > nth ? (n / nth) : 1;
+    #pragma omp single
+    {
+      block = n > nth ? (n / nth) : 1;
+    }
+
+    #pragma omp paralle for shared(paths, numpaths) collapse(2)
 		for (int i = me*block; i < (me + 1)*block && i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (adjm[i * n + j] == 1) // Jump to row j
-				{
+				if (adjm[i * n + j] == 1) {
 					int depth = 0;
 					int visited[k + 1];
 					visited[depth] = i;
@@ -61,46 +57,4 @@ void findpaths(int *adjm, int n, int k, int *paths, int *numpaths)
 			}
 		}
 	}
-}
-
-int main(void)
-{
-	int n = 3;
-	int k = 2;
-
-	int *adjm = new int[n * n];
-
-	for (int i = 0; i < n * n; i++)
-		adjm[i] = 0;
-
-	adjm[1] = 1;
-	adjm[3] = 1;
-	adjm[5] = 1;
-	adjm[6] = 1;
-
-	int size = 300;
-	int numpaths, paths[size];
-
-	for (int i = 0; i < size; i++)
-		paths[i] = -1;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cout << adjm[i * n + j] << " ";
-		}
-		cout << endl;
-	}
-	findpaths(adjm, n, k, paths, &numpaths);
-	cout << numpaths << endl;
-
-	for (int i = 0; i < numpaths; i++)
-	{
-		for (int j = 0; j < k + 1; j++)
-		{
-			cout << paths[i * (k + 1) + j] << " ";
-		}
-		cout << endl;
-	}
-
-	return 0;
 }
