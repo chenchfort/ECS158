3decc4900e9b144f9bd65db213118af067271748
willxcience <wilwu@ucdavis.edu>
2016-05-11 20:41:13 -0700

    xxxx (#3)

--- HW3/nmf.R
+++ HW3/nmf.R
@@ -1,8 +1,8 @@
-nmfgpu <- function(a,k)
+nmfgpu <- function(a,k, niters)
 {
   #following code depends on the machine
   system("/usr/local/cuda/bin/nvcc -g -Xcompiler -fPIC -c nmfgpur.cu -arch=sm_13 -I/usr/include/R")
   system("R CMD SHLIB nmfgpur.o -lcudart -L/usr/local/cuda/lib64")
   dyn.load("nmfgpur.so")
-  .Call("gpu",a, as.integer(k))
+  .Call("gpu",a, as.integer(k), as.integer(niters))
 }
